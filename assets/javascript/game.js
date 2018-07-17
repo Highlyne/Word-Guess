b739f1819506af27220dd98c4a759a955c3e3644
Tamara Hall <tamarahall@Tamaras-MBP.lan>
2018-07-17 19:47:41 -0400

    adding pseudo code for the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,16 @@
 
-
-
-
-
-
     console.log("This page is conectted");
+
+    // 1. Set cumputer word bank
+    // 2. Have Computer pick a word randomly from the word bank
+    // 3. Dispaly a dash for the letters of the word chosen 
+    // 4. Grab the user's key that is pressed
+    // 5. Check if that key matches a character in the word that was picked by the Computer
+    // 6. Show the letter to the screen that was picked
+    // 7. If the letter is correct then show the letter instead of the dash
+    // 8. Decrease the number of guesses 
+    // 9. If the number of guesses drops to zero then stop the game
+    // 10. If the entire word has been guessed then add to the number of wins
+    // 11. If word was not guessed then increase the losses 
+    // 12. The the user have the option to restart the game
 
